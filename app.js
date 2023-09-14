976470ac4464cfeb4b4c875ab37aaf738c92397b
Nazari <aliahmadnazari13@gmail.com>
2023-09-14 22:11:57 +0200

    ejs templating is installed and home-page is rendered, also stylesheet file is created

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const express = require('express');
 require('dotenv').config();
 
+
 const app = express();
 
+//////////////////////////////// Setting View Engine ////////////////////////////////
+app.set('view engine', 'ejs');
+
+//////////////////////////////// Making static / public folder /////////////////////
+app.use(express.static('public'));
 
 /////////////////////////////// Home Page /////////////////////////////////////////
 
 app.get('/', (req, res) => {
-    res.send('Herzlich welcommen');
+    res.render('index', { title: 'Home' });
 });
 
 
 
 
-app.listen(process.env.PORT || 5000);
+app.listen(process.env.PORT || 5000, process.env.HOST || 'localhost', () => {
+    console.log(`Server is running on http://${process.env.HOST}:${process.env.PORT}`);
+});
