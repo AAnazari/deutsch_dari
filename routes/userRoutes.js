d3bd91a8ed47b3f8fd19d8ac31f5ece624cae476
Nazari <aliahmadnazari13@gmail.com>
2023-10-12 12:02:26 +0200

    Joi is installed and 'User Register Form' is Validated. Also Flass Messages in login and register pagges are configured

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const userController = require('../controllers/userController');
 const userRoute = express.Router();
 
-
 ///////////////////////////////////// Profile get route //////////////////////////////////
 userRoute.get('/', userController.profile_get);
 
